cb48ee4fa28fa9aaeb3e14b86ff527bbf46d5165
HKotsev <hristian.kotsev@c4nexus.com>
2023-11-15 09:40:17 +0200

    extending the imageTransformation

--- storefront-reference-architecture-master/cartridges/app_storefront_PD/cartridge/experience/components/commerce_assets/banner.js
+++ storefront-reference-architecture-master/cartridges/app_storefront_PD/cartridge/experience/components/commerce_assets/banner.js
@@ -14,11 +14,7 @@ module.exports.render = function (context) {
 
    var model = new HashMap();
 
-   // add parameters to model as required for rendering based on the given context (dw.experience.ComponentScriptContext):​
 
-   // * context.component (dw.experience.Component)​
-
-   // * context.content (dw.util.Map)​
    model.title=context.content.title;
    model.image = ImageTransformation.getScaledImage(context.content.image);
    model.src = context.content.src;
